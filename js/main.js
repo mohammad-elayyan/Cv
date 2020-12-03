ebba5562d070ecc9955a978a16b4b337a0e8ef36
mohammad-elayyan <45799946+mohammad-elayyan@users.noreply.github.com>
2020-12-04 01:00:33 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){
+
+    $("button").click(function(){
+        $(".burger").toggleClass("show");
+        $(".CContainerWrap").toggleClass("position");
+        $("footer").toggleClass("footer");
+    });
+
+var typed = new Typed('.typedHome',{
+    strings:["Web Developer","Web Disgner","Freelancer","Photographer"],
+    typeSpeed:60,
+    loop:true
+});
+
+
+let pie = function(p){
+$(p).easyPieChart({
+    size:140,
+    scaleColor:false,
+    trackColor:'#DCE8F8',
+    lineWidth:5,
+    animate:5000
+});
+};
+
+let nCount = function (selector) {
+    $(selector).each(function () {
+        $(this).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 5000,
+            easing: "swing",
+            step: function (value) {
+                $(this).text(Math.ceil(value))
+            }
+        });
+    });
+};
+
+let a = 0;
+$("#CContainer").scroll(function () {
+    let oTop = $(".chart").offset().top;
+    if (a == 0 && $(this).scrollTop() >= oTop) {
+        a++;
+        nCount('.percent');
+        pie('.chart');
+    }
+    if(innerHeight-$("#Home").offset().top >= 790){
+        $(".top").css("display","flex");
+    }
+    else{
+        $(".top").css("display","none");
+    }
+});
+
+AOS.init();
+
+});
